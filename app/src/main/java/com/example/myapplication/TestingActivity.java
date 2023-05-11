02cb688a1b634492c3db18f4c14c54d1a1de9c0b
alexlkv <alexlkv77@gmail.com>
2023-05-11 07:17:13 +0300

    update

--- app/src/main/java/com/example/myapplication/TestingActivity.java
+++ app/src/main/java/com/example/myapplication/TestingActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +26,7 @@ import okhttp3.Response;
 
 public class TestingActivity extends AppCompatActivity {
 
+    private boolean cheked = false;
     private int index = 0;
     private int mlength = 10;
     private List<String> answers = new ArrayList<>();
@@ -31,7 +34,7 @@ public class TestingActivity extends AppCompatActivity {
     private TextView textViewQuestion;
     private List<Button> answerButtons = new ArrayList<>();
 
-
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +48,41 @@ public class TestingActivity extends AppCompatActivity {
         answerButtons.add(findViewById(R.id.btn_ans_4));
 
         loadQuestion();
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                cheked = true;
+                switch (checkedId) {
+                    case -1:
+                        Toast.makeText(getApplicationContext(), "Ничего не выбрано",
+                                Toast.LENGTH_SHORT).show();
+                        cheked = false;
+                        break;
+                    case R.id.btn_ans_1:
+                        Toast.makeText(getApplicationContext(), "Первый переключатель",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_ans_2:
+                        Toast.makeText(getApplicationContext(), "Второй переключатель",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_ans_3:
+                        Toast.makeText(getApplicationContext(), "Третий переключатель",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.btn_ans_4:
+                        Toast.makeText(getApplicationContext(), "Четвертый переключатель",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     private void loadQuestion() {
@@ -66,7 +104,6 @@ public class TestingActivity extends AppCompatActivity {
                 try {
                     JSONObject jsonObject = new JSONObject(jsonData);
                     Question question = new Gson().fromJson(jsonObject.getJSONArray("questions").getJSONObject(index).toString(), Question.class);
-
                     answers.clear();
                     answers.addAll(question.getAnswers());
 
@@ -87,6 +124,8 @@ public class TestingActivity extends AppCompatActivity {
             }
         });
     }
+
+
     private static class Question {
         private String level;
         private String id;
@@ -117,9 +156,14 @@ public class TestingActivity extends AppCompatActivity {
 
     public void next(View view) {
 
-        if (index < mlength) {
+        if (index < mlength && cheked) {
             index++;
+            loadQuestion();
+            cheked = false;
+            radioGroup.clearCheck();
+        } else if (!cheked) {
+            Toast.makeText(getApplicationContext(), "Ничего не выбрано",
+                    Toast.LENGTH_SHORT).show();
         } else index = 0;
-        loadQuestion();
     }
 }
