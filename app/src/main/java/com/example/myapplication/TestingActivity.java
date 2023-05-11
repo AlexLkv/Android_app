3c2eaaa1cd986b7602a62d2711bb7ef2f9270dab
alexlkv <alexlkv77@gmail.com>
2023-05-12 01:48:23 +0300

    update

--- app/src/main/java/com/example/myapplication/TestingActivity.java
+++ app/src/main/java/com/example/myapplication/TestingActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +19,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import okhttp3.Call;
@@ -25,62 +29,53 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class TestingActivity extends AppCompatActivity {
-
+    Question question;
+    boolean right_ans = false;
     private boolean cheked = false;
     private int index = 0;
-    private int mlength = 10;
-    private List<String> answers = new ArrayList<>();
-
-    private TextView textViewQuestion;
+    private int count_right_ans = 0;
+    private final int mlength = 10;
+    private TextView textViewQuestion, textIndex, text_right_ans;
+    private RadioGroup radioGroup;
     private List<Button> answerButtons = new ArrayList<>();
 
-    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
 
-        textViewQuestion = findViewById(R.id.question);
-
-        answerButtons.add(findViewById(R.id.btn_ans_1));
-        answerButtons.add(findViewById(R.id.btn_ans_2));
-        answerButtons.add(findViewById(R.id.btn_ans_3));
-        answerButtons.add(findViewById(R.id.btn_ans_4));
-
+        initializeUI();
         loadQuestion();
+        setListeners();
+    }
 
+    private void initializeUI() {
         radioGroup = findViewById(R.id.radioGroup);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        text_right_ans = findViewById(R.id.right_ans);
+        textViewQuestion = findViewById(R.id.question);
+        textIndex = findViewById(R.id.index);
+        answerButtons.addAll(Arrays.asList(findViewById(R.id.btn_ans_1), findViewById(R.id.btn_ans_2), findViewById(R.id.btn_ans_3), findViewById(R.id.btn_ans_4)));
+    }
 
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                cheked = true;
-                switch (checkedId) {
-                    case -1:
-                        Toast.makeText(getApplicationContext(), "Ничего не выбрано",
-                                Toast.LENGTH_SHORT).show();
-                        cheked = false;
-                        break;
-                    case R.id.btn_ans_1:
-                        Toast.makeText(getApplicationContext(), "Первый переключатель",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.btn_ans_2:
-                        Toast.makeText(getApplicationContext(), "Второй переключатель",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.btn_ans_3:
-                        Toast.makeText(getApplicationContext(), "Третий переключатель",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.btn_ans_4:
-                        Toast.makeText(getApplicationContext(), "Четвертый переключатель",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-
-                    default:
-                        break;
-                }
+    private void setListeners() {
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            cheked = true;
+            switch (checkedId) {
+                case -1:
+                    cheked = false;
+                    break;
+                case R.id.btn_ans_1:
+                    right_ans = question.getRight_ans().equals("1");
+                    break;
+                case R.id.btn_ans_2:
+                    right_ans = question.getRight_ans().equals("2");
+                    break;
+                case R.id.btn_ans_3:
+                    right_ans = question.getRight_ans().equals("3");
+                    break;
+                case R.id.btn_ans_4:
+                    right_ans = question.getRight_ans().equals("4");
+                    break;
             }
         });
     }
@@ -103,18 +98,14 @@ public class TestingActivity extends AppCompatActivity {
 
                 try {
                     JSONObject jsonObject = new JSONObject(jsonData);
-                    Question question = new Gson().fromJson(jsonObject.getJSONArray("questions").getJSONObject(index).toString(), Question.class);
-                    answers.clear();
-                    answers.addAll(question.getAnswers());
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            for (int i = 0; i < answerButtons.size(); i++) {
-                                answerButtons.get(i).setText(answers.get(i));
-                            }
-                            textViewQuestion.setText(question.getQuestion());
+                    question = new Gson().fromJson(jsonObject.getJSONArray("questions").getJSONObject(index).toString(), Question.class);
+                    runOnUiThread(() -> {
+                        for (int i = 0; i < answerButtons.size(); i++) {
+                            answerButtons.get(i).setText(question.getAnswers().get(i));
                         }
+                        textViewQuestion.setText(question.getQuestion());
+                        textIndex.setText("Номер вопроса: " + (index + 1));
+                        text_right_ans.setText("Верных ответов: " + count_right_ans);
                     });
 
 
@@ -131,7 +122,10 @@ public class TestingActivity extends AppCompatActivity {
         private String id;
         private String right_ans;
         private String question;
-        private List<String> answers;
+        private List<String> answers = new ArrayList<>();
+
+        private Question() {
+        }
 
         public String getLevel() {
             return level;
@@ -156,14 +150,34 @@ public class TestingActivity extends AppCompatActivity {
 
     public void next(View view) {
 
+        AlertDialog.Builder builder = null;
         if (index < mlength && cheked) {
+            if (right_ans) {
+                count_right_ans++;
+                text_right_ans.setText("Верных ответов: " + count_right_ans);
+            }
+            textIndex.setText("Номер вопроса: " + (index + 1));
             index++;
             loadQuestion();
             cheked = false;
             radioGroup.clearCheck();
         } else if (!cheked) {
-            Toast.makeText(getApplicationContext(), "Ничего не выбрано",
-                    Toast.LENGTH_SHORT).show();
-        } else index = 0;
+            Toast.makeText(getApplicationContext(), "Ничего не выбрано", Toast.LENGTH_SHORT).show();
+        }else {
+        builder = new AlertDialog.Builder(TestingActivity.this);
+        builder.setTitle("Поздравляем! Вы прошли тестирование");
+        builder.setMessage("Количество вопросов: " + (index + 1) + "\n" + "Количество верных ответов: " + count_right_ans );
+
+        builder.setPositiveButton("Ок, пон",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Intent intent = new Intent(TestingActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+        builder.show();
+
     }
-}
+
+}}
